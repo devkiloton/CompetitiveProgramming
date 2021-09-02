9b918119cd368b0c7aa63676ad88151f934a67fc
devkiloton <dev.kiloton@gmail.com>
2021-09-01 23:24:13 -0300

    Codeforces_677A

---
+++ CodeForces/Problem_677A/677A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+namespace std;
+
+int main (void) {
+    string nh;
+    int n;
+    int h;
+    //A minimun width to the friends walk side to side with their heads in a row!
+    //Normal width = 1/ bent person = 2
+    //The height of i-th person is ai -> (1 <= ai <= 2h)
+    //n = number of friends
+    //h = height of the fence
+    cin >> nh;
+    cout << nh[0] << nh[1] << nh[2];
+}
